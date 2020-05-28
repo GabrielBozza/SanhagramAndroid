ecfbc3f1aa1d023b3dea8f095f662ab1527163e3
GabrielBozza <gbozza2@gmail.com>
2020-05-27 21:57:10 -0300

    Tela de conversas completa e tela do chat funcional, mas o layout desconfigura ao subir o teclado

--- app/src/main/java/com/example/aulalabprogiii/ChatUsuario.java
+++ app/src/main/java/com/example/aulalabprogiii/ChatUsuario.java
@@ -3,8 +3,14 @@ package com.example.aulalabprogiii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,7 +25,8 @@ import cz.msebera.android.httpclient.Header;
 
 public class ChatUsuario extends AppCompatActivity {
 
-    TextView corpochat;
+    Button BotaoEnviar, Texto_Mensagem;
+    EditText Mensagem;
     String PrefixoURL = "http://192.168.15.5:8080";//PARTE QUE MUDA QUANDO EU USO O LACALHOST RUN (PARA Q PESSOAS DE FORA DA MINHA REDE POSSAM ACESSAR)
     String IdentificadorURL = "/SanhagramServletsJSP/UsuarioControlador?acao=listarConversas&dispositivo=android";
     String URL = PrefixoURL+IdentificadorURL;
@@ -33,26 +40,66 @@ public class ChatUsuario extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_usuario);
 
-        TextView corpochat = (TextView)findViewById(R.id.corpochat);
+        Mensagem = (EditText)findViewById(R.id.Mensagem);
+        BotaoEnviar = (Button)findViewById(R.id.BotaoEnviar);
 
         String resultado = getIntent().getStringExtra("MensagensConversa");
-
-        String Mensagens ="";
+        String login = getIntent().getStringExtra("Login");
 
         try {
 
             JSONObject response = new JSONObject(resultado);
 
+            GradientDrawable Bolha_esquerda = new GradientDrawable();
+            Bolha_esquerda.setColor(getResources().getColor(R.color.colorBolhaEsquerda));
+            Bolha_esquerda.setShape(GradientDrawable.RECTANGLE);
+            Bolha_esquerda.setCornerRadii(new float[]{5, 5, 25, 25, 25, 25, 25, 25});
+
+            GradientDrawable Bolha_direita = new GradientDrawable();
+            Bolha_direita.setColor(getResources().getColor(R.color.colorBolhaDireita));
+            Bolha_direita.setShape(GradientDrawable.RECTANGLE);
+            Bolha_direita.setCornerRadii(new float[]{25, 25, 5, 5, 25, 25, 25, 25});
+
             try {
                 for(int i=0;i<response.getJSONArray("MENSAGENS").length();i++) {
-                    //TextView mensagem = new TextView(TelaCadastro.this);
-                    //mensagem.setText(response.getJSONArray("MENSAGENS").getJSONObject(i).get("texto_mensagem").toString());
-                    Mensagens += response.getJSONArray("MENSAGENS").getJSONObject(i).get("remetente").toString();
-                    Mensagens += " : ";
-                    Mensagens += response.getJSONArray("MENSAGENS").getJSONObject(i).get("texto_mensagem").toString();
-                    Mensagens += " - ";
-                    Mensagens += response.getJSONArray("MENSAGENS").getJSONObject(i).get("data_envio").toString().substring(11,16);
-                    Mensagens += "\n";
+
+                    String remetente = response.getJSONArray("MENSAGENS").getJSONObject(i).get("remetente").toString();
+                    String texto = response.getJSONArray("MENSAGENS").getJSONObject(i).get("texto_mensagem").toString();
+                    String hora_envio = response.getJSONArray("MENSAGENS").getJSONObject(i).get("data_envio").toString().substring(11,16);
+                    String bolha_mensagem_esquerda = " "+remetente+"       "+hora_envio+"\n"+" "+texto;
+                    String bolha_mensagem_direita = "    "+hora_envio+"\n "+texto+" ";
+
+                    LinearLayout layout = (LinearLayout) findViewById(R.id.ConversaUsuario);
+                    Texto_Mensagem = new Button(this);
+
+                    Texto_Mensagem.setTransformationMethod(null);
+                    Texto_Mensagem.setTextColor(getResources().getColor(R.color.white));
+                    Texto_Mensagem.setMaxWidth(900);
+                    if(remetente.equals(login)) {
+                        Texto_Mensagem.setBackground(Bolha_direita);
+                        Texto_Mensagem.setText(bolha_mensagem_direita);
+                        Texto_Mensagem.setGravity(Gravity.RIGHT);
+                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT);
+                        params.gravity=Gravity.RIGHT;
+                        params.setMargins(16,10,8,10);
+                        Texto_Mensagem.setLayoutParams(params);
+
+                    }
+                    else{
+                        Texto_Mensagem.setBackground(Bolha_esquerda);
+                        Texto_Mensagem.setText(bolha_mensagem_esquerda);
+                        Texto_Mensagem.setGravity(Gravity.LEFT);
+                        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                                LinearLayout.LayoutParams.WRAP_CONTENT);
+                        params.setMargins(8,10,16,10);
+                        Texto_Mensagem.setLayoutParams(params);
+                    }
+                    Texto_Mensagem.setPadding(10,10,10,10);
+                    Texto_Mensagem.setTextSize(14);
+                    Texto_Mensagem.setClickable(false);
+
+                    layout.addView(Texto_Mensagem);
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -60,8 +107,6 @@ public class ChatUsuario extends AppCompatActivity {
         }catch (JSONException err){
             Log.d("Error", err.toString());
         }
-
-        corpochat.setText(Mensagens);
 
 
     }
@@ -85,6 +130,52 @@ public class ChatUsuario extends AppCompatActivity {
                 Intent intent = new Intent( getApplicationContext(), ListaConversas.class);
                 intent.putExtra("Login",login);
                 intent.putExtra("ListaConversas",json.toString());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+
+                Toast.makeText(ChatUsuario.this, "Erro!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+
+    public void EnviarMensagem(View view) {
+
+        String texto_mensagem = Mensagem.getText().toString();
+        final String remetente = getIntent().getStringExtra("Login");
+        final String destinatario = getIntent().getStringExtra("Destinatario");
+
+        if (texto_mensagem.matches("")) {
+            Toast.makeText(this, "Escreva alguma coisa!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String URLenviar = "http://192.168.15.5:8080/SanhagramServletsJSP/UsuarioControlador?acao=enviarMsgm&dispositivo=android" + "&remetente="
+                + remetente + "&destinatario=" + destinatario + "&texto_mensagem="+texto_mensagem;
+
+        params = new RequestParams();
+        params.put("remetente",remetente);
+        params.put("destinatario",destinatario);
+        params.put("texto_mensagem",texto_mensagem);
+
+        client = new AsyncHttpClient();
+        client.post(URLenviar,params, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+
+                json = response;
+
+                Intent intent = new Intent(getApplicationContext(), ChatUsuario.class);
+                intent.putExtra("MensagensConversa", json.toString());
+                intent.putExtra("Login", remetente);
+                intent.putExtra("Destinatario", destinatario);
                 startActivity(intent);
             }
 
--- app/src/main/java/com/example/aulalabprogiii/ListaConversas.java
+++ app/src/main/java/com/example/aulalabprogiii/ListaConversas.java
@@ -3,8 +3,10 @@ package com.example.aulalabprogiii;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -27,14 +29,12 @@ public class ListaConversas extends AppCompatActivity {
     AsyncHttpClient client;
     JSONObject json;
 
-    Button Conversa, Chat;
+    Button Chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_conversas);
-
-        Conversa = (Button) findViewById(R.id.Conversa);
 
         String resultado = getIntent().getStringExtra("ListaConversas");
         final String login = getIntent().getStringExtra("Login");
@@ -43,6 +43,12 @@ public class ListaConversas extends AppCompatActivity {
 
             JSONObject response = new JSONObject(resultado);
 
+            GradientDrawable Opcao_Conversa = new GradientDrawable();
+            Opcao_Conversa.setColor(getResources().getColor(R.color.Conversa)); // Changes this drawbale to use a single color instead of a gradient
+            Opcao_Conversa.setShape(GradientDrawable.RECTANGLE);
+            Opcao_Conversa.setCornerRadii(new float[]{25, 25, 25, 25, 25, 25, 25, 25});
+
+
             try {
                 for (int i = 0; i < response.getJSONArray("CONVERSAS").length(); i++) {
 
@@ -50,7 +56,19 @@ public class ListaConversas extends AppCompatActivity {
                     LinearLayout layout = (LinearLayout) findViewById(R.id.Listaconversas);
                     Chat = new Button(this);
                     Chat.setText(amigo);
-                    Chat.setId(i+1);
+                    Chat.setTransformationMethod(null);
+                    Chat.setMaxWidth(600);
+                    Chat.setPadding(10,10,10,10);
+                    Chat.setTextSize(16);
+                    Chat.setBackground(Opcao_Conversa);
+                    Chat.setGravity(Gravity.CENTER);
+
+                    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT);
+                    params.setMargins(16,10,16,10);
+                    params.gravity=Gravity.CENTER;
+                    Chat.setLayoutParams(params);
+                    Chat.setMinimumWidth(600);
 
                     Chat.setOnClickListener(new View.OnClickListener() {
                         @Override
@@ -69,52 +87,11 @@ public class ListaConversas extends AppCompatActivity {
             Log.d("Error", err.toString());
         }
 
-        Conversa.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                String remetente = "gbozza";
-                String destinatario = "mbozza";
-
-                URL = URL + "&remetente=" + remetente + "&destinatario=" + destinatario;
-
-                client = new AsyncHttpClient();
-                client.get(URL, new JsonHttpResponseHandler() {
-
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                        super.onSuccess(statusCode, headers, response);
-
-                        json = response;
-
-                        Intent intent = new Intent(getApplicationContext(), ChatUsuario.class);
-                        intent.putExtra("MensagensConversa", json.toString());
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                        super.onFailure(statusCode, headers, responseString, throwable);
-
-                        Toast.makeText(ListaConversas.this, "Erro!", Toast.LENGTH_LONG).show();
-
-                    }
-
-                    @Override
-                    public void onFinish() {
-                        super.onFinish();
-
-                    }
-                });
-            }
-        });
-
     }
 
     public void VerChat(String a, String b) {
         final String remetente = a;
-        String destinatario = b;
+        final String destinatario = b;
 
         URL = URL + "&remetente=" + remetente + "&destinatario=" + destinatario;
 
@@ -130,6 +107,7 @@ public class ListaConversas extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(), ChatUsuario.class);
                 intent.putExtra("MensagensConversa", json.toString());
                 intent.putExtra("Login", remetente);
+                intent.putExtra("Destinatario", destinatario);
                 startActivity(intent);
             }
 
